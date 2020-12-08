de13ba0d9c1877bc10ee3fa0316bb482a5e19c98
Lunerio <lunerio@outlook.es>
2020-12-08 18:39:20 +0000

    add file 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,4 +5,4 @@ for i in range(10):
             if i != 8 and j != 9:
                 print('{}{}'.format(i, j), end=', ')
             elif i is 8 and j is 9:
-                print('{}{}'.format(i,j))
+                print('{}{}'.format(i, j))
