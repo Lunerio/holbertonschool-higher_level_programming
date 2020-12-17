688d2b959c7165625a4c3c003bdcf75e158daea0
Lunerio <lunerio@outlook.es>
2020-12-17 01:45:38 +0000

    repair 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,8 +3,8 @@ def max_integer(my_list=[]):
     if my_list:
         max = 0
         for i in my_list:
-            if my_list[i] > max:
-                max = my_list[i]
+            if i > max:
+                max = i
         return max
     else:
         return None
