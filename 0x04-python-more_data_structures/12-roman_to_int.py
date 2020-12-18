dd7b24487808090af4e1c076088498ceea3dc7fa
Lunerio <lunerio@outlook.es>
2020-12-18 00:36:36 +0000

    test 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string == None or type(roman_string) != str:
+    if not roman_string or type(roman_string) != str:
         return None
 
     values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
@@ -17,8 +17,6 @@ def roman_to_int(roman_string):
         for a, b in values.items():
             if i == a:
                 str_list.append(b)
-
-    print(str_list)
 
     for i in range(len(str_list)):
         sum = sum + str_list[i]
