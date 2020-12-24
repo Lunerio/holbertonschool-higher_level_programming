98fb3b9ce7c40b54143c2888056e525bf6825d8e
Lunerio <lunerio@outlook.es>
2020-12-24 20:00:07 +0000

    repair 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    char = sentence[0]
+    char = ""
     if length == 0:
         char = None
+    else:
+        char = sentence[0]
     new_t = (length, char)
     return new_t
