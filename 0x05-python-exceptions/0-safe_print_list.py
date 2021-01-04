5402389106e0d631128e7ea1060273da4214ac21
Lunerio <lunerio@outlook.es>
2021-01-04 16:18:25 +0000

    repair 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,6 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             count += 1
-    except(IndexError):
-        pass
-    print()
+    except IndexError as err:
+        print("IndexError:", err)
     return count
