601b873712c2a88cea45b6df1dea8d8d925897bc
Lunerio <lunerio@outlook.es>
2021-01-04 15:12:46 +0000

    repair 0 and 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print("{0}".format(my_list[i]), end="")
+        print()
+        return (i + 1)
     except(IndexError):
         print()
         return i
-    print()
-    return i + 1
