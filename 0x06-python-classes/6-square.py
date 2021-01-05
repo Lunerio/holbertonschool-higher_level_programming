63175c879af1d449dede45bc49e7556e547e67ff
Lunerio <lunerio@outlook.es>
2021-01-05 20:54:40 +0000

    test 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """square with conditions"""
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """init with the conditions for the attribute"""
         self.__size = size
         if type(size) is not int:
