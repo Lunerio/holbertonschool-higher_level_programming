942bd04593fa8d406eef684fcfc247b78c808886
Lunerio <lunerio@outlook.es>
2021-01-05 15:42:07 +0000

    coso 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
         for i in range(x):
-            print("{0}".format(my_list[i]), end="")
-    except(IndexError):
-        print()
-        return i
+            print("{}" .format(my_list[i]), end="")
+            count += 1
+    except:
+        pass
     print()
-    return i + 1
+    return count
