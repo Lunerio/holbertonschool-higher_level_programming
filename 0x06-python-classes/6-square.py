d000c0d3baf46033f046286dcdeacab450de6881
Lunerio <lunerio@outlook.es>
2021-01-05 20:43:53 +0000

    que pasa con el 6?

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,13 +4,14 @@
 
 class Square:
     """square with conditions"""
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """init with the conditions for the attribute"""
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__position = position
 
     @property
     def size(self):
@@ -25,6 +26,17 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
+    @property
+    def position(self):
+        """getter for position"""
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        if len(position) != 2 and
+        (type(position[0]) is not int and type(position[1]) is not int):
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     def my_print(self):
         """print square"""
