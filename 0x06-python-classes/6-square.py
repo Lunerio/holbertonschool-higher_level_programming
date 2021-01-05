25bfddddb9d968d993087ef684c712646d0a4edb
Lunerio <lunerio@outlook.es>
2021-01-05 20:52:39 +0000

    ya fue todo

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,14 +4,13 @@
 
 class Square:
     """square with conditions"""
-    def __init__(self, size=0, position=(0, 0)):
+    def __init__(self, size=0):
         """init with the conditions for the attribute"""
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__position = position
 
     @property
     def size(self):
@@ -26,18 +25,6 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-
-    @property
-    def position(self):
-        """getter for position"""
-        return self.__position
-
-    @position.setter
-    def position(self, position):
-        """ufa bo"""
-        if len(position) != 2 and
-        type(position[0]) is not int and type(position[1]) is not int:
-            raise TypeError("position must be a tuple of 2 positive integers")
 
     def my_print(self):
         """print square"""
