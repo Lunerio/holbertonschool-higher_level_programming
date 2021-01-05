e7bfc1537c98dacf31e7999108fb6765aab7ef7a
Lunerio <lunerio@outlook.es>
2021-01-05 16:22:00 +0000

    add comments for 0, 1 and 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class Square:
+    """this does initiate the object with a private attribute"""
     def __init__(self, size):
+        """init method"""
         self.__size = size
