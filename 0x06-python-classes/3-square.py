4bc7ccd9c54c33c43c799a56c496cb8b471f5901
Lunerio <lunerio@outlook.es>
2021-01-05 16:27:57 +0000

    ad file 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    """square with conditions"""
+    def __init__(self, size=0):
+        """init with the conditions for the attribute"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """method for returning area of square"""
+        a = self.__size
+        return a * a
