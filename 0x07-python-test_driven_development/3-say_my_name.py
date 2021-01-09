ec0b55ac6a13573ca21905c80b8c1a10302e98be
Lunerio <lunerio@outlook.es>
2021-01-09 01:43:23 +0000

    save 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module prints in screen a first name and a last name.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+
+    """
+    This function prints two strings together, and checks possible problems.
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
