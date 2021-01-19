06c6f367002fbf34003dcd4861478878e9353af5
Lunerio <lunerio@outlook.es>
2021-01-19 20:49:39 +0000

    add tests for 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module with a class"""
+
+
+class MyList(list):
+    """tremendo suquistruquis"""
+    def print_sorted(self):
+        """prints a sorted list"""
+        print (sorted(self))
