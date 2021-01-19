c0d4c83bfdfb79d9f1b8f0af71af7e26ea8a0485
Lunerio <lunerio@outlook.es>
2021-01-19 22:36:04 +0000

    repair 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,6 +5,7 @@ This module contains a class called BaseGeometry"""
 
 Rectangle = __import__("9-rectangle").Rectangle
 
+
 class Square(Rectangle):
     """subclass of Rectangle"""
     def __init__(self, size):
