c9c3df4bb3e5c205fa56d764379da49470671ee5
Lunerio <lunerio@outlook.es>
2021-01-19 21:32:50 +0000

    dale 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,4 +11,4 @@ def inherits_from(obj, a_class):
     return true if subclass,
     false if not
     """
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
