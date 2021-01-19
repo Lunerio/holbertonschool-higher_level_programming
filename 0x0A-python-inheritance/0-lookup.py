e44118a14f245d15ad8913425584b504eb2f747d
Lunerio <lunerio@outlook.es>
2021-01-19 15:40:01 +0000

    first push

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module has a method that returns
+a list with the dictionary of an object
+"""
+
+
+def lookup(obj):
+    """
+    return a list with the dict of obj
+    """
+
+    return (list(obj.__dict__))
