705f4a7e19e0964bf4d27326f889367e972f8b8a
Lunerio <lunerio@outlook.es>
2021-01-19 21:20:59 +0000

    repair 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,7 @@ from a given class
 
 def inherits_from(obj, a_class):
     """return true if subclass, false if not"""
-    if isinstance(obj, a_class) and type(obj) != a_class:
+    if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     else:
         return False
