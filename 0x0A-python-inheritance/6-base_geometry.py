ea451b00c94b1088bfd857adb6058ab5f6b0f605
Lunerio <lunerio@outlook.es>
2021-01-19 17:08:20 +0000

    add file 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains a class called BaseGeometry"""
+
+
+class BaseGeometry():
+    """contains a method"""
+    def area(self):
+        """raise error"""
+        raise Exception("area() is not implemented")
