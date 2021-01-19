763b66b81bea64f7efe69dba181a3abcaf4d58a4
Lunerio <lunerio@outlook.es>
2021-01-19 21:41:26 +0000

    test 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-"""
-this module contains a function that checks
-if an object is an instance of a subclass that inherited
-from a given class
-"""
+"""module for a function"""
 
 
 def inherits_from(obj, a_class):
-    """
-    return true if subclass,
-    false if not
-    """
-
+    """ true if subclass, false if not"""
     return issubclass(type(obj), a_class) and type(obj) is not a_class
