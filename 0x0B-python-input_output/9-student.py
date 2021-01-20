f76ad0b23fa5272ec481d5dbb00e807077dd5f09
Lunerio <lunerio@outlook.es>
2021-01-20 18:12:48 +0000

    add file 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/pyton3
+"""module for a class declaration"""
+
+
+class Student():
+    """class student.
+    with public attributes
+    and method
+    """
+    first_name = None
+    last_name = None
+    age = None
+
+    def __init__(self, first_name, last_name, age):
+        """init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """return dict of instance"""
+        return self.__dict__
