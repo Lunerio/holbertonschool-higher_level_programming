e6a0677927f612f3695757c0eb0924de5ba78479
Lunerio <lunerio@outlook.es>
2021-01-20 18:53:45 +0000

    add file 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/pyton3
+"""module for a class declaration"""
+
+
+class Student():
+    """class student.
+    with public attributes
+    and method
+    """
+    first_name = None
+    last_name = None
+    age = None
+
+    def __init__(self, first_name, last_name, age):
+        """init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return dict of instance"""
+        new_dict = {}
+        if attrs == None:
+            return self.__dict__
+        for i in attrs:
+            if hasattr(self, i):
+                new_dict[i] = getattr(self, i)
+        return new_dict
