ec11661bd6a46553e2eb73696c7f50c361b12a9d
Lunerio <lunerio@outlook.es>
2021-01-20 14:57:09 +0000

    file 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module with function that uses json"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """json module for data to string"""
+    return json.dumps(my_obj)
