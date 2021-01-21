429c5d607e90f2df6518e9994099c5083ea289bb
Lunerio <lunerio@outlook.es>
2021-01-21 06:06:37 +0000

    save 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -30,18 +30,37 @@ def pascal_triangle(n):
 
     for i in range(2, n):
         index = 1
-        limite = int((len(lista_base) + 2) / 2)
 
-        for j in range(0, limite - 1):
+        if (len(lista_base) % 2) == 1:
+            limite = int((len(lista_base) + 2) / 2)
             save_list = lista_base[:]
-            new_num = save_list[j] + save_list[j + 1]
-            save_list.insert(index, new_num)
-            lista_base = save_list[:]
-            index += 1
 
-        for j in range(-1, (limite * -1) - 1, -1):
-            lista_base[j] = lista_base[(j * -1) - 1]
+            for j in range(0, limite - 1):
+                new_num = lista_base[j] + lista_base[j + 1]
+                save_list.insert(index, new_num)
+                index += 1
+
+            lista_base = save_list[:]
+
+            for j in range(-1, (limite * -1) - 1, -1):
+                lista_base[j] = lista_base[(j * -1) - 1]
+
+        else:
+            limite = len(lista_base) - 1
+            save_list = lista_base[:]
+
+            for j in range(0, limite):
+                new_num = lista_base[j] + lista_base[j + 1]
+                save_list.insert(index, new_num)
+                index += 1
+
+            lista_base = save_list[:]
+
+            for j in range(-1, (limite * -1), -1):
+                lista_base.pop(j - 1)
+                lista_base[j] = lista_base[(j * -1) - 1]
 
         lista_big.append(lista_base)
+
 
     return lista_big
