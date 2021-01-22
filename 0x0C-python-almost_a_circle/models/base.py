5c24878dc54d4842ba9e060804559e0b20c4a57f
Lunerio <lunerio@outlook.es>
2021-01-22 15:53:08 +0000

    add first push

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+module with a base class
+"""
+
+
+class Base():
+    """base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """init method"""
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
