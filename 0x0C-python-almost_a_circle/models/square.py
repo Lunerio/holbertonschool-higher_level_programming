01d6a847e6d1afbddc7773f1b886afe92432ba7a
Lunerio <lunerio@outlook.es>
2021-01-23 18:40:50 +0000

    save 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,24 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """init method"""
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
+        """string method"""
         return ("[Square] ({}) {}/{} - {}".format
                 (self.id, self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        """getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """setter for size"""
+        if type(size) is not int:
+            raise TypeError("width must be an integer")
+        if size <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = self.size
+        self.__height = self.size
