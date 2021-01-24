0e724b58edcddf6293d3946159afb75dbfdc7560
Lunerio <lunerio@outlook.es>
2021-01-24 01:25:55 +0000

    save

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,10 +35,22 @@ class Base():
         """use json to write to a file a list of instances"""
         if type(list_objs) is not list:
             raise TypeError("argument must be a list")
-        name = "{cls}.json"
+
         lista = []
+
         for i in list_objs:
             lista.append(i.__dict__)
-        name = "Rectangle.json"
+
+        name = str(cls.__name__) + ".json"
+
         with open(name, 'w') as f:
             json.dump(lista, f)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """return json string representation as list"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        if type(json_string) is not str:
+            raise TypeError("argument must be a string")
+        return list(json.loads(json_string))
