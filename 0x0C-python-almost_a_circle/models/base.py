0b8507747b284483b2313330c53dcd5c870cf755
Lunerio <lunerio@outlook.es>
2021-01-24 20:23:05 +0000

    save

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,7 +7,6 @@ module with the Base class
 import json
 
 
-
 class Base():
     """base class"""
     __nb_objects = 0
@@ -35,16 +34,16 @@ class Base():
         """use json to write to a file a list of instances"""
         if type(list_objs) is not list:
             raise TypeError("argument must be a list")
-
         lista = []
-
-        for i in list_objs:
-            lista.append(cls.to_dictionary(i))
-
         name = str(cls.__name__) + ".json"
-
-        with open(name, 'w') as f:
-            f.write(cls.to_json_string(lista))
+        if list_objs is None:
+            with open(name, 'w') as f:
+                f.write(cls.to_json_string(lista))
+        else:
+            for i in list_objs:
+                lista.append(cls.to_dictionary(i))
+            with open(name, 'w') as f:
+                f.write(cls.to_json_string(lista))
 
     @staticmethod
     def from_json_string(json_string):
@@ -61,3 +60,21 @@ class Base():
         instance = cls(1, 1, 1, 1)
         cls.update(instance, **dictionary)
         return instance
+
+    @classmethod
+    def load_from_file(cls):
+        """return list of instances of class read from a file"""
+        filename = str(cls.__name__) + ".json"
+        lista_big = []
+        lista_dicts = []
+        text = ""
+        try:
+            with open(filename, 'r') as f:
+                text = f.read()
+            lista_dicts = cls.from_json_string(text)
+            for i in lista_dicts:
+                inst = cls.create(**i)
+                lista_big.append(inst)
+            return lista_big
+        except IOError:
+            return []
