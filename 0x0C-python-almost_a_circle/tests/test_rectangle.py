81fd147d3326b712a1fd6d7ff39eccbbd2ee975b
Lunerio <lunerio@outlook.es>
2021-01-25 14:24:14 +0000

    save

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -10,6 +10,7 @@ class TestRectangle(unittest.TestCase):
     """testing the Rectangle class and its methods"""
 
     def test_inst_methods(self):
+        """test methods"""
         test_inst = Rectangle(2, 3, 0, 0, 1)
         """test area of rectangle"""
         self.assertEqual(test_inst.area(), 6)
@@ -18,9 +19,36 @@ class TestRectangle(unittest.TestCase):
         """test to_dictionary"""
         self.assertEqual(test_inst.to_dictionary(),
                          {'id': 1, 'width': 2, 'height': 3, 'x': 0, 'y': 0})
-        """test display"""
-        self.assertMultiLineEqual(test_inst.display(), '##\n##\n##\n')
 
+    def test_negatives(self):
+        """test negative values"""
+        with self.assertRaises(ValueError):
+            """negative width"""
+            fail_inst = Rectangle(-2, 3, 3, 0, 0)
+        with self.assertRaises(ValueError):
+            """negative height"""
+            fail_inst = Rectangle(2, -3, 3, 0, 0)
+        with self.assertRaises(ValueError):
+            """negative x"""
+            fail_inst = Rectangle(2, 3, -1, 0, 0)
+        with self.assertRaises(ValueError):
+            """negative y"""
+            fail_inst = Rectangle(-2, 3, 0, -2, 0)
+
+    def test_not_int(self):
+        """test values without ints"""
+        with self.assertRaises(TypeError):
+            """on width"""
+            fail_inst = Rectangle('testStr', 3, 0, 0, 1)
+        with self.assertRaises(TypeError):
+            """on height"""
+            fail_inst = Rectangle(3, '3', 0, 0, 1)
+        with self.assertRaises(TypeError):
+            """on x"""
+            fail_inst = Rectangle(1, 3, '0', 0, 1)
+        with self.assertRaises(TypeError):
+            """on y"""
+            fail_inst = Rectangle(2, 3, 0, '0', 1)
 
 if __name__ == '__main__':
     unittest.main()
