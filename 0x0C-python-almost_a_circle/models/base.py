e54d1055b5ad051ed302b350a7017e589099525b
Lunerio <lunerio@outlook.es>
2021-01-27 13:10:08 +0000

    test base 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,8 +24,6 @@ class Base():
         """return json string object of argument"""
         if type(list_dictionaries) is not list:
             raise TypeError("argument must be a list")
-        if list_dictionaries is not None:
-            raise TypeError("argument must be a list")
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
