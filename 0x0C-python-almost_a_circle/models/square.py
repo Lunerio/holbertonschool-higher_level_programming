0e1e7e50315ad09e8e1fbe5113e125b69c8f2d9c
Lunerio <lunerio@outlook.es>
2021-01-27 13:50:11 +0000

    algo

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,15 +19,11 @@ class Square(Rectangle):
     @property
     def size(self):
         """getter for size"""
-        return self.width, self.height
+        return self.width
 
     @size.setter
     def size(self, value):
         """setter for size"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
