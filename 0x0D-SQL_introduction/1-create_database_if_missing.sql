be7ae7adea7af403294a305adfff445ebb899b1e
Lunerio <lunerio@outlook.es>
2021-03-01 12:21:00 -0300

    typos make the difference

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- creates a database only if it does not exist
 -- in case that it exists it will not have problems
-CREATE DATABASE IF NOT EXISTS `htbn_0c_0`;
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- deletes a database if it exists. if not it does not fail
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
