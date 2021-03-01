98f60a21c4d67cd8ee1c756b95ce3ab23a90ebba
Lunerio <lunerio@outlook.es>
2021-03-01 12:14:35 -0300

    repair 1?

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
--- creates a database
-CREATE DATABASE [IF NOT EXISTS] htbn_0c_0;
+-- creates a database only if it does not exist
+-- in case that it exists it will not have problems
+CREATE DATABASE IF NOT EXISTS htbn_0c_0;
