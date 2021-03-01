b296c100afa0c3b242e50e8632786b4eec35827a
Lunerio <lunerio@outlook.es>
2021-03-01 20:17:46 -0300

    repair 101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- show the average temp on each place
-SELECT city, AVG(value) 'avg_temp' FROM temperatures GROUP BY city;
+SELECT city, AVG(value) 'avg_temp' FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
