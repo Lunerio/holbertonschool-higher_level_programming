b6e4a1a5eed4dd367c61eeb021ad7b8fbec13a50
Lunerio <lunerio@outlook.es>
2021-03-01 20:00:30 -0300

    ripeir sien

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
 -- changes database, table and field name to UTF9 encoding
 -- utf8mb4_unicode_ci
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE htbn_0c_0;
+USE hbtn_0c_0;
 ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utfb8mb4 COLLATE utf8mb4_unicode_ci;
