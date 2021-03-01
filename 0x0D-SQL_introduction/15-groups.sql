84365bb395d63cc7f73155b417afddfe58623f70
Lunerio <lunerio@outlook.es>
2021-03-01 15:24:44 -0300

    test 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- count the rows with the same score values
-SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score;
