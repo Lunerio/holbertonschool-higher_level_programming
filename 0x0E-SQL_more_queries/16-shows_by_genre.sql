1cfbe8d892a595a73d05905e06f2b15e6e02530e
Lunerio <lunerio@outlook.es>
2021-03-02 20:04:08 -0300

    save 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,4 @@
+-- list all shows and related genres linked to the show
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id;
+LEFT JOIN tv_genres ON tv_show_genres.genre.id = tv_genres.id;
