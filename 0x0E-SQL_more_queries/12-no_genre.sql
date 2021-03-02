9a1fa672886c5015079b3299c4fd57a60753e873
Lunerio <lunerio@outlook.es>
2021-03-02 15:00:36 -0300

    save 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,6 +3,6 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT OUTER JOIN tv_show_genres
-ON tv_show_genres.genre_id = tv_shows.id
+ON tv_shows.id=tv_show_genres.show_id
 WHERE show_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
