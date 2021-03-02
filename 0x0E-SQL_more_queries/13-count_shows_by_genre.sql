424936f7d8c8edc1309ddc468afc7b162bbe5252
Lunerio <lunerio@outlook.es>
2021-03-02 19:44:04 -0300

    mira vos che

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_of_shows FROM tv_genres
 LEFT JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY name
-ORDER BY number_of_shows DESC;
+GROUP BY tv_genres.name
+ORDER BY COUNT(tv_genres.name) DESC;
