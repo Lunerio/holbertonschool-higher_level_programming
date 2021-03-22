a19788c9b04e96c9cb0b2cb5d68fc50099219332
Lunerio <lunerio@outlook.es>
2021-03-22 16:11:56 -0300

    add file 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function add(a, b) {
+  const val = a + b;
+  return val;
+}
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+const value = add(a, b);
+console.log(value);
