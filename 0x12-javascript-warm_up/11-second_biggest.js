b7c749995913a2804c0199d95d39e30d37b5563a
Lunerio <lunerio@outlook.es>
2021-03-22 18:03:58 -0300

    repair 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,13 +3,7 @@ const argLen = (process.argv.length) - 2;
 if (argLen === 0 || argLen === 1) {
   console.log(0);
 } else {
-  let biggest = parseInt(process.argv[2]);
-  let prev = parseInt(process.argv[2]);
-  for (let i = 2; i < process.argv.length; i++) {
-    if (parseInt(process.argv[i]) > biggest) {
-      prev = biggest;
-      biggest = parseInt(process.argv[i]);
-    }
-  }
-  console.log(prev);
+  const prev = process.argv;
+  prev.sort(function (a, b) { return a - b; });
+  console.log(prev[prev.length - 2]);
 }
