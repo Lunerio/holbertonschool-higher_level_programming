29b0c005096736929644c2b3d058ce2a75a5a68d
Lunerio <lunerio@outlook.es>
2021-03-27 10:31:07 -0300

    test 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-"""This module connects to a database and prints rows
-   argv[1] = username
-   argv[2] = password
-   argv[3] = database name
-   argv[4] = Data filter
-"""
-
-
-if __name__ == "__main__":
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
+if __name__ == '__main__':
     from sys import argv
     import MySQLdb
-    # connect to a database
-    database = MySQLdb.connect(host="localhost", port=3306,
-                               user=argv[1], passwd=argv[2], db=argv[3])
-    # create cursor
-    cursor = database.cursor()
-    # execute query
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE " +
                 "BINARY '{}'".format(argv[4]))
-    # fetch result into variable
-    rows = cursor.fetchall()
-    # print rows
-    for i in rows:
-        print('{}'.format(i))
-    # close cursor and database
-    cursor.close()
-    database.close()
+    rows = cur.fetchall()
+    for row in rows:
+        print("{}".format(row))
+    cur.close()
+    db.close()
