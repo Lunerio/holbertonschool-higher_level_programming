44fc1ea0b93f802beaa387f315d961d917882586
Lunerio <lunerio@outlook.es>
2021-04-08 13:06:44 -0300

    repair 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Get a response from the given argument
-curl -X PUT "$1" -L -H "Origin: HolbertonSchool" -d "user_id=98"
+curl -s -X PUT "$1" -L -H "Origin: HolbertonSchool" -d "user_id=98"
