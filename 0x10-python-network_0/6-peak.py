843a1f30edd3d01687eaf4db9ad95db7fb57e7c4
Lunerio <lunerio@outlook.es>
2021-04-08 17:43:08 -0300

    add conditions 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,8 +6,13 @@
 
 def find_peak(list_of_integers):
     """Function that finds a peak in a list"""
-    if len(list_of_integers) < 3:
+    if len(list_of_integers) == 0:
         return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
+
     save = list_of_integers[1]
     for i in range(1, len(list_of_integers) - 1):
         if list_of_integers[i] >= list_of_integers[i - 1] and\
