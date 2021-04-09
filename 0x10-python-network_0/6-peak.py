55c296d9564d60f1c642a256f4ef7fb99cef341a
Lunerio <lunerio@outlook.es>
2021-04-08 21:39:59 -0300

    test again

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,10 +13,12 @@ def find_peak(list_of_integers):
     elif len(list_of_integers) == 2:
         return max(list_of_integers)
     else:
-        mid = int((len(list_of_integers) - 1) // 2)
+        mid = int(len(list_of_integers) // 2)
 
         if list_of_integers[mid] >= list_of_integers[mid - 1] and\
                 list_of_integers[mid] >= list_of_integers[mid + 1]:
             return list_of_integers[mid]
-        return find_peak(list_of_integers[mid+1:])
-        return find_peak(list_of_integers[:mid-1])
+        if list_of_integers[mid + 1] > list_of_integers[mid]:
+            return find_peak(list_of_integers[mid + 1:])
+        if list_of_integers[mid - 1] > list_of_integers[mid]:
+            return find_peak(list_of_integers[:mid - 1])
