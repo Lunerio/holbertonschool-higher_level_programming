c8c327335d7e1b8b728e2d988153d994684f9673
Lunerio <lunerio@outlook.es>
2021-04-09 01:24:19 -0300

    add 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This module takes an URL and uses the POST request
+    To set the email variable with a given argument
+"""
+
+
+from sys import argv
+import urllib.request
+import urllib.parse
+
+
+if __name__ == '__main__':
+    var = urllib.parse.urlencode({"email": argv[2]})
+    var = var.encode('utf8')
+    with urllib.request.urlopen(argv[1], var) as response:
+        http_info = response.read()
+
+    print(http_info.decode())
