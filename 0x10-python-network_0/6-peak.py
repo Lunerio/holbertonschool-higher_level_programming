4a803059b0973e211173ed325f74b820b44d7a41
Lunerio <lunerio@outlook.es>
2021-04-08 21:17:06 -0300

    test 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,14 +8,18 @@ def find_peak(list_of_integers):
     """Function that finds a peak in a list"""
     if len(list_of_integers) == 0:
         return None
-    if len(list_of_integers) == 1:
+    elif len(list_of_integers) == 1:
         return list_of_integers[0]
-    if len(list_of_integers) == 2:
+    elif len(list_of_integers) == 2:
         return max(list_of_integers)
+    else:
+        hi = len(list_of_integers) - 1
+        lo = 0
+        mid = int(hi // 2)
 
-    save = list_of_integers[1]
-    for i in range(1, len(list_of_integers) - 1):
-        if list_of_integers[i] >= list_of_integers[i - 1] and\
-                list_of_integers[i] >= list_of_integers[i + 1]:
-            save = list_of_integers[i]
-    return save
+        if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+            return list_of_integers[mid]
+        else:
+            return find_peak(list_of_integers[mid+1:])
+            return find_peak(list_of_integers[:mid-1])
