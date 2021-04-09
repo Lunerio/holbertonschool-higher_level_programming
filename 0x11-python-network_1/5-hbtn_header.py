9ee2421c21f2a43c146bd2ac8238966d1defa739
Lunerio <lunerio@outlook.es>
2021-04-09 01:53:11 -0300

    save 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that uses requests package
+"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    response = requests.get(argv[1])
+    print(response.headers['X-Request-Id'])
