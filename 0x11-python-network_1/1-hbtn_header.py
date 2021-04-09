14c42739fb55f8fd59c7ca7a3a002c6a482b933b
Lunerio <lunerio@outlook.es>
2021-04-09 01:08:49 -0300

    save

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Request an URL from argument and display the value of
+    X-Request-Id from the header
+"""
+
+
+from sys import argv
+import urllib.request
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io') as response:
+        http = response.getheader('X-Request-Id')
+
+    print(http)
