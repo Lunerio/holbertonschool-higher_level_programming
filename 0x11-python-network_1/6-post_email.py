3c1133def9f187b5044b987e353f8adc6a2b7119
Lunerio <lunerio@outlook.es>
2021-04-09 01:58:06 -0300

    add 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that uses the POST method from requests package
+"""
+
+
+from sys import argv
+import requests
+
+
+if __name__ == '__main__':
+    response = requests.post(argv[1], {'email': argv[2]})
+    print(response.text)
