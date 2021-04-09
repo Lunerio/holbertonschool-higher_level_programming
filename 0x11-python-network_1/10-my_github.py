e011f5387e3cb2578da0a8f431485a809411e453
Lunerio <lunerio@outlook.es>
2021-04-09 13:34:23 -0300

    add 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This module uses requests to get information from a user
+    from GITHUB API
+"""
+
+
+from sys import argv
+import requests
+
+
+if __name__ == '__main__':
+    response = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    dict = response.json()
+    print('{}'.format(dict.get('id')))
