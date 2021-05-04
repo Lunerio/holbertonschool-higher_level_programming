381cb5c9f0dc9df6b80d42dee9147905408850a9
Lunerio <lunerio@outlook.es>
2021-05-04 13:20:53 -0300

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,9 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
-
-request('https://jsonplaceholder.typicode.com/todos', function (error, response, body) {
-    let tasks = JSON.parse(body);
+const url = process.argv[2];
+const dict = {};
+request(url, function (error, response, body){
+  if (error) {
+    console.error(error);
+  } else {
+    const tasks = JSON.parse(body);
     for (let i = 0; i < tasks.length; i++) {
-	console.log(tasks[i].title);
+      const key = tasks[i].userId;
+      if (tasks[i].completed) {
+        if (!(key in dict)) {
+          dict[key] = 1;
+        } else {
+          dict[key] += 1;
+        }
+      }
     }
+  }
+  console.log(dict);
 });
