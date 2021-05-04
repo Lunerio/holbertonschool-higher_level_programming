cd0829fe7737b84518bb14fa112be9c6025bd8ef
Lunerio <lunerio@outlook.es>
2021-05-04 01:49:46 -0300

    add file 3

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,7 @@ const URL = process.argv[2];
 request(URL, function (error, response) {
   if (error) {
     console.error(error);
+  } else {
+    console.log('code:', response && response.statusCode);
   }
-  console.log('code:', response && response.statusCode);
 });
