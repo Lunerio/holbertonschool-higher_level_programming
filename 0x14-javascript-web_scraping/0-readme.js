beb2cee2c0bef1ef673071ea270ef351796bdf89
Lunerio <lunerio@outlook.es>
2021-05-04 00:54:26 -0300

    repair semi

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-let file_path = process.argv[2]
-fs = require('fs')
-fs.readFile(file_path, 'utf8', function (err,data) {
-    if (err) {
-	return console.log(err);
-    }
-    console.log(data);
+const filePath = process.argv[2];
+const fs = require('fs');
+fs.readFile(filePath, 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
 });
